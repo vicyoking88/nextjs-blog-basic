61932fa5fafdd2df923c63abb2776803d567a5a9
Victor Medina <victor.medina@qcode.co>
2022-02-02 21:09:33 -0500

    children_contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,8 @@ import Layout from "../components/Layout";
 const contact = () => {
   return (
       <Layout title="Contacto | next.js"
-      description="agrega una descripcion de contacto">
+      description="agrega una descripcion de contacto"
+      home={false}>
           <div><h1>web contacts</h1></div>
       </Layout>
  );
