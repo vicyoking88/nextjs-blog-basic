de47aa22e997debbc59f4bb3ce9f15c4a374661b
Victor Medina <victor.medina@qcode.co>
2022-02-02 21:04:23 -0500

    children_about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,8 @@ import Layout from "../components/Layout";
 
 export default function about() {
   return <Layout title="About | next.js"
-  description="agrega una descripcion de about">
+  description="agrega una descripcion de about"
+  home={false}>
       <h1>ABOUT</h1>
   </Layout>;
 }
